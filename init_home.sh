a61b2b5418549c6a990e1df7be652756b673d7e2
Panos Mavrogiorgos <pmav99@gmail.com>
2020-02-27 12:38:10 +0100

    init_home: Use full path for ipython

--- init_home.sh
+++ init_home.sh
@@ -4,7 +4,7 @@ set -xeuo pipefail
 
 mkdir -p ~/.config/
 mkdir -p ~/.config/direnv
-mkdir -p ~/.config/ipython
+mkdir -p ~/.config/ipython/profile_default/
 mkdir -p ~/.config/pgcli
 mkdir -p ~/.config/pypoetry
 mkdir -p ~/.local/bin
