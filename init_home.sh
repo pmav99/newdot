8d6175ce0a74f2d43ee1f1095bdc5349a5ed5eda
Panos Mavrogiorgos <pmav99@gmail.com>
2020-04-04 14:49:44 +0300

    Init: Ensure that ~/Prog exists

--- init_home.sh
+++ init_home.sh
@@ -2,9 +2,10 @@
 
 set -xeuo pipefail
 
+mkdir -p ~/Prog/
 mkdir -p ~/.config/
-mkdir -p ~/.config/direnv
+mkdir -p ~/.config/direnv/
 mkdir -p ~/.config/ipython/profile_default/
-mkdir -p ~/.config/pgcli
-mkdir -p ~/.config/pypoetry
-mkdir -p ~/.local/bin
+mkdir -p ~/.config/pgcli/
+mkdir -p ~/.config/pypoetry/
+mkdir -p ~/.local/bin/
