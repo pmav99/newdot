c28fe2b127c38fd9c353d82000a81f6a1f161b8b
Panos Mavrogiorgos <pmav99@gmail.com>
2023-02-10 07:14:17 +0200

    Add kitty configuration

--- init_home.sh
+++ init_home.sh
@@ -6,6 +6,7 @@ mkdir -p ~/Prog/
 mkdir -p ~/.config/
 mkdir -p ~/.config/direnv/
 mkdir -p ~/.config/ipython/profile_default/
+mkdir -p ~/.config/kitty/
 mkdir -p ~/.config/pgcli/
 mkdir -p ~/.config/pypoetry/
 mkdir -p ~/.local/bin/
