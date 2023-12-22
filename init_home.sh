08d4dd05b25648d75cebb097ac17730c96f3c392
Panos Mavrogiorgos <pmav99@gmail.com>
2023-12-22 18:58:27 +0200

    Update init_home.sh

--- init_home.sh
+++ init_home.sh
@@ -8,6 +8,7 @@ mkdir -p ~/.config/direnv/
 mkdir -p ~/.config/ipython/profile_default/
 mkdir -p ~/.config/ipython/profile_default/startup/
 mkdir -p ~/.config/kitty/
+mkdir -p ~/.config/my_notifications/
 mkdir -p ~/.config/nvim/
 mkdir -p ~/.config/pgcli/
 mkdir -p ~/.config/pypoetry/
