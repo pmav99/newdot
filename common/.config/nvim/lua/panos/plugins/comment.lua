0b9dcd01d7a31bc6ad654075ce714197007a2b8d
Panos Mavrogiorgos <pmav99@gmail.com>
2024-01-30 11:26:02 +0200

    nvim: Fix comment keybindings

--- common/.config/nvim/lua/panos/plugins/comment.lua
+++ common/.config/nvim/lua/panos/plugins/comment.lua
@@ -4,19 +4,31 @@ return {
   dependencies = {
     -- "JoosepAlviste/nvim-ts-context-commentstring",
   },
-  keys = {
-    { "q", mode = { "n", "x" }, desc = " Comment Operator" },
-    { "Q", desc = " Append Comment at EoL" },
-    { "qo", desc = " Comment below" },
-    { "qO", desc = " Comment above" },
-  },
+  -- keys = {
+  --   { "q", mode = { "n", "x" }, desc = " Comment Operator" },
+  --   { "Q", desc = " Append Comment at EoL" },
+  --   { "qo", desc = " Comment below" },
+  --   { "qO", desc = " Comment above" },
+  -- },
   opts = {
-    opleader = { line = "q", block = "<Nop>" },
-    toggler = { line = "qq", block = "<Nop>" },
-    extra = { eol = "Q", above = "qO", below = "qo" },
+    opleader = {
+        line = '<leader>C',
+        block = '<leader>B',
+    },
+    toggler = {
+        line = '<leader>c',
+        block = '<leader>b',
+    },
+    extra = {
+      above = '<leader>CO',
+      below = '<leader>Co',
+      eol = '<leader>CA',
+    }
+    -- opleader = { line = "q", block = "<Nop>" },
+    -- toggler = { line = "qq", block = "<Nop>" },
+    -- extra = { eol = "Q", above = "qO", below = "qo" },
   },
 }
-
 
 -- keymap.set(
 --   "n",
--- common/.config/nvim/lua/panos/plugins/lsp/lspconfig.lua
+++ common/.config/nvim/lua/panos/plugins/lsp/lspconfig.lua
@@ -35,7 +35,7 @@ return {
       keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
 
       opts.desc = "See available code actions"
-      keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
+      keymap.set({ "n", "v" }, "<leader>za", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
 
       opts.desc = "Smart rename"
       keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
