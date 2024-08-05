8fa25b5a7ec3d628e8f69fc6a7a25d96bdd773b7
Panos Mavrogiorgos <pmav99@gmail.com>
2024-08-05 13:19:48 +0300

    ipython: Add custom magic commands for `bat` and `la`

---
+++ common/.config/ipython/profile_default/startup/custom_magics.py
@@ -0,0 +1,16 @@
+from IPython.core.magic import register_line_magic as _register_line_magic
+#from IPython.core.magic import register_cell_magic as _register_cell_magic
+#from IPython.core.magic import register_line_cell_magic as _register_line_cell_magic
+
+
+@_register_line_magic
+def bat(line):
+    "Call `bat`"
+    return get_ipython().system(f"bat {line}")
+
+@_register_line_magic
+def la(line):
+    "Call `ls -lah`"
+    return get_ipython().run_line_magic("ls", f" -lah {line}")
+
+del bat, la
