eb6c07755e8b3bf4e98872452ba841da9c2570f3
Panos Mavrogiorgos <pmav99@gmail.com>
2024-08-26 11:37:51 +0300

    ipython: Fix highlight of tracebacks

--- common/.config/ipython/profile_default/ipython_config.py
+++ common/.config/ipython/profile_default/ipython_config.py
@@ -1,3 +1,10 @@
+# Setup Traceback for dark themes
+# https://github.com/ipython/ipython/pull/14138
+# https://github.com/ipython/ipython/issues/14464
+# https://stylishthemes.github.io/Syntax-Themes/pygments/
+from IPython.core.ultratb import VerboseTB
+VerboseTB._tb_highlight_style = "monokai"
+
 c.TerminalInteractiveShell.editing_mode = 'vi'
 c.TerminalInteractiveShell.emacs_bindings_in_vi_insert_mode = False
 c.InteractiveShell.ast_node_interactivity = "all"
