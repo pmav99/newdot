1c405f7f9d2e169c0240d6ddc56fa2638f633779
Panos Mavrogiorgos <pmav99@gmail.com>
2025-04-21 09:08:05 +0300

    ipython: Fix Background on Tracebacks

--- common/.config/ipython/profile_default/ipython_config.py
+++ common/.config/ipython/profile_default/ipython_config.py
@@ -3,7 +3,10 @@
 # https://github.com/ipython/ipython/issues/14464
 # https://stylishthemes.github.io/Syntax-Themes/pygments/
 from IPython.core.ultratb import VerboseTB
-VerboseTB._tb_highlight_style = "monokai"
+
+VerboseTB.tb_highlight = "bg:#500060"
+VerboseTB.tb_highlight_style = "monokai"
+# VerboseTB.tb_highlight_style = "gruvbox-light"
 
 c.TerminalInteractiveShell.editing_mode = 'vi'
 c.TerminalInteractiveShell.emacs_bindings_in_vi_insert_mode = False
